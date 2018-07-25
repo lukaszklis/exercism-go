d407b27cc7e9b7c534f63b0f9f53177022bfb730
Lukasz Klis <lukasz@klis.pl>
2018-07-25 12:30:54 +0200

    :zap: Add "Two Fer" task

---
+++ two-fer/two_fer.go
@@ -0,0 +1,6 @@
+// Package twofer should have a package comment that summarizes what it's about.
+package twofer
+
+// ShareWith should have a comment documenting it.
+func ShareWith(string) string {
+}
